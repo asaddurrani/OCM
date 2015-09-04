0c9a0cd4d62de7f498b906dc497898a04321bafb
asad032 <asadkhan9@outlook.com>
2015-09-04 13:58:06 -0700

    attempt1

--- Implementation/Identity/IdentityOperation.cs
+++ Implementation/Identity/IdentityOperation.cs
@@ -1,38 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Dynamic;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
-using Interfaces.Repository;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
-using Microsoft.Owin.Security;
 using Microsoft.Practices.Unity;
-using Models.DomainModels;
 using Models.IdentityModels;
 using Repository.BaseRepository;
-
-using Microsoft.Owin.Host.SystemWeb;
-
-using System.Linq;
-using System.Security.Claims;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin;
-using Microsoft.Owin.Security;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Threading.Tasks;
-using System.Web;
-using Models.IdentityModels;
-using Repository.Repositories;
 using System.Configuration;
 
 
